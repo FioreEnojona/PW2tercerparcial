abc4b216ee2ae5260ce8b8c987a5ed2d0018bcb9
FioreEnojona <estoyaquiporobligacion@gmail.com>
2025-04-05 19:45:51 -0600

    b

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.DEPLOY_BASE_URL,
+
   plugins: [
     react(),
     tailwindcss(),
